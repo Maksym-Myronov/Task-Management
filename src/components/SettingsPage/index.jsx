2e38a823175a23421d259326b14d6a7d1bb39091
Maksym-Myronov <maksym.myronov2018@gmail.com>
2023-10-13 17:17:01 +0300

    add settings info page

--- src/components/SettingsPage/index.jsx
+++ src/components/SettingsPage/index.jsx
@@ -1,9 +1,42 @@
 import styles from './index.module.scss'
+import boy from '../../assets/images/boy.png'
 
 const SettingsPage = () => {
     return (
-        <div className={styles.name}>
-        <h1>hi</h1>
+        <div className={styles.settingsContainer}>
+            <div>
+                <h1 className={styles.shippingInfo}>Personal account settings</h1>
+            </div>
+            <div>
+                <img src={boy} alt="boy" className={styles.imagesUser} />
+            </div>
+            <div className={styles.genderBtn}>
+                <div className={styles.btnContainer}>
+                    <button className={styles.takeGenderBtn}>Male</button>
+                    <button className={styles.takeGenderBtn}>Female</button>
+                </div>
+            </div>
+            <div className={styles.formData}>
+                <div className={styles.inputInfo}>
+                    <p>UserName</p>
+                    <input type="text" placeholder="UserName" className={styles.inputBox} />
+                </div>
+                <div className={styles.inputInfo}>
+                    <p>Email</p>
+                    <input type="text" placeholder="Email"  className={styles.inputBox}  />
+                </div>
+                <div className={styles.inputInfo}>
+                    <p>Country</p>
+                    <input type="text" placeholder="Country" className={styles.inputBox} />
+                </div>
+                <div className={styles.inputInfo}>
+                    <p>City</p>
+                    <input type="text" placeholder="City" className={styles.inputBox} />
+                </div>
+            </div>
+            <div>
+                <button className={styles.saveInfo}>Save information</button>
+            </div>
         </div>
     )
 }
